5672bd601cf5e3ccfe997efc053c053d39b9c2a0
Hermann Hueck <hermann.hueck@mailbox.org>
2020-01-08 14:55:20 +0100

    Added FutureExample

---
+++ scala3-migration/src/main/scala/migrate2dotty/future/FutureExample.scala
@@ -0,0 +1,39 @@
+package migrate2dotty.future
+
+import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.duration._
+import scala.util.{Failure, Success, Try}
+
+def someComputation(): Int =
+  println("computing Int value asynchronously ...")
+  Thread.sleep(2000L)
+  42
+
+def defTryHandler[A](tryy: Try[A]): Unit = {
+  tryy match
+    case Success(value)     => s"value = ${value.toString}"
+    case Failure(throwable) => s"exception = ${throwable.getMessage}"
+} pipe println
+
+val tryHandler = defTryHandler[Int] // eta expansion
+
+
+import scala.util.chaining._
+import scala.language.implicitConversions
+import util._
+
+@main def FutureExample: Unit =
+
+  line().green pipe println
+
+  import ExecutionContext.Implicits.{given ExecutionContext}
+  // given ExecutionContext = ExecutionContext.global
+
+  val future: Future[Int] = Future { someComputation() }
+
+  future onComplete tryHandler
+
+  Thread.sleep(3000L)
+
+  line().green pipe println
+
