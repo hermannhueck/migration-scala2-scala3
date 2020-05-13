71c624b5086270a3e23badfcb246f51b3bee8eea
Hermann Hueck <hermann.hueck@mailbox.org>
2020-05-13 17:09:18 +0200

    added debug macro from Adam Warski' blog

---
+++ scala3-migration/src/main/scala/migrate2dotty/macro/use/TestDebugMacro.scala
@@ -0,0 +1,39 @@
+package migrate2dotty.macros.use
+
+// See:
+//   https://blog.softwaremill.com/starting-with-scala-3-macros-a-short-tutorial-88e9d2b2584c
+//   https://github.com/softwaremill/scala3-macro-debug
+
+import scala.util.chaining._
+import migrate2dotty.macros.define.Debug._
+import util._
+import scala.language.implicitConversions
+
+object TestDebugMacro extends App:
+
+  s"${line(10)} inline hello:" pipe println
+
+  hello()
+
+  val z = 2
+
+  def test =
+    val x = 0
+    val y = 1
+
+    s"${line(10)} inline debugSingle:" pipe println
+
+    debugSingle(x)
+    debugSingle(x+y)
+
+    s"${line(10)} inline debug:" pipe println
+
+    debug(x)
+    debug(x, y)
+    debug(x+y)
+    debug(x, x+y)
+    debug("A", x, x+y)
+    debug("A", x, "B", y)
+    debug(x, y, z)
+
+  test
